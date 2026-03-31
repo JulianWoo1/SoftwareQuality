305e64b2b10307d65b70461c371828e36c5116a6
Julian Woo <julian.woo@student.nhlstenden.com>
2026-03-31 09:49:33 +0200

    added more tests

--- test/com/nhlstenden/jabberpoint/Serializer/SlideLoaderVisitorTest.java
+++ test/com/nhlstenden/jabberpoint/Serializer/SlideLoaderVisitorTest.java
@@ -1,15 +1,22 @@
 package com.nhlstenden.jabberpoint.Serializer;
 
 import com.nhlstenden.jabberpoint.Presentation;
+import com.nhlstenden.jabberpoint.Slide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SlideLoaderVisitorTest
 {
-    private SlideItemFactory factory;
+    private SlideLoaderVisitor visitor;
     private Presentation presentation;
 
     @BeforeEach
@@ -17,6 +24,14 @@ class SlideLoaderVisitorTest
     {
         presentation = Presentation.getInstance();
         presentation.clearPresentation();
+        visitor = new SlideLoaderVisitor(presentation, new DefaultSlideItemFactory());
+    }
+
+    private Element parseElement(String xml) throws Exception
+    {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+        return doc.getDocumentElement();
     }
 
     @Test
@@ -29,5 +44,93 @@ class SlideLoaderVisitorTest
 
         assertEquals(1, presentation.getSlides().size());
     }
+
+    @Test
+    void testVisitItem_ValidXML_ShouldAddSlideItem() throws Exception
+    {
+        Element itemElement = parseElement("<item kind=\"text\" level=\"1\">Hello</item>");
+
+        visitor.visitSlide(parseElement("<slide/>"));
+        visitor.visitItem(itemElement);
+
+        assertEquals(1, presentation.getSlides().get(0).getSlideItems().size());
+    }
+
+    @Test
+    void testVisitShowTitle_ShouldSetPresentationTitle() throws Exception
+    {
+        Element showTitle = parseElement("<showtitle>My Presentation</showtitle>");
+        visitor.visitShowTitle(showTitle);
+        assertEquals("My Presentation", presentation.getTitle());
+    }
+
+    @Test
+    void testVisitSlide_ShouldAddNewSlide() throws Exception
+    {
+        Element slideElement = parseElement("<slide/>");
+        visitor.visitSlide(slideElement);
+        assertEquals(1, presentation.getSlides().size());
+    }
+
+    @Test
+    void testVisitSlide_TwoCalls_ShouldAddTwoSlides() throws Exception
+    {
+        Element slideElement = parseElement("<slide/>");
+        visitor.visitSlide(slideElement);
+        visitor.visitSlide(slideElement);
+        assertEquals(2, presentation.getSlides().size());
+    }
+
+    @Test
+    void testVisitTitle_WithActiveSlide_ShouldSetSlideTitle() throws Exception
+    {
+        visitor.visitSlide(parseElement("<slide/>"));
+        visitor.visitTitle(parseElement("<title>Slide One</title>"));
+
+        assertEquals("Slide One", presentation.getSlides().get(0).getTitle());
+    }
+
+    @Test
+    void testVisitTitle_WithoutActiveSlide_ShouldNotThrow() throws Exception
+    {
+        visitor.visitPresentation(parseElement("<presentation/>"));
+
+        assertDoesNotThrow(() ->
+                visitor.visitTitle(parseElement("<title>No Slide</title>"))
+        );
+    }
+
+    @Test
+    void testVisitItem_WithoutActiveSlide_ShouldNotAddItem() throws Exception
+    {
+        visitor.visitPresentation(parseElement("<presentation/>"));
+        visitor.visitItem(parseElement("<item kind=\"text\" level=\"1\">Orphan</item>"));
+
+        assertTrue(presentation.getSlides().isEmpty());
+    }
+
+    @Test
+    void testVisitPresentation_ShouldResetActiveSlide() throws Exception
+    {
+        visitor.visitSlide(parseElement("<slide/>"));
+        visitor.visitPresentation(parseElement("<presentation/>"));
+
+        visitor.visitTitle(parseElement("<title>Should not be set</title>"));
+
+        assertNull(presentation.getSlides().get(0).getTitle());
+    }
+
+    @Test
+    void testVisitItem_TextItem_ShouldHaveCorrectLevelAndContent() throws Exception
+    {
+        visitor.visitSlide(parseElement("<slide/>"));
+        visitor.visitItem(parseElement("<item kind=\"text\" level=\"3\">Detail text</item>"));
+
+        Slide slide = presentation.getSlides().get(0);
+        assertEquals(1, slide.getSlideItems().size());
+        assertEquals(3, slide.getSlideItems().get(0).getLevel());
+        assertEquals("Detail text", slide.getSlideItems().get(0).getText());
+    }
+
 
 }
--- test/com/nhlstenden/jabberpoint/Serializer/XMLSerializerTest.java
+++ test/com/nhlstenden/jabberpoint/Serializer/XMLSerializerTest.java
@@ -46,6 +46,47 @@ class XMLSerializerTest
     }
 
     @Test
+    void testSaveAndLoad_SlideWithTextItem_ShouldRestoreItemLevelAndContent() throws Exception
+    {
+        Slide slide = new Slide();
+        slide.setTitle("slide with item");
+        slide.addSlideItem(new TextItem(2, "Hello World"));
+        presentation.getSlides().add(slide);
+        presentation.setTitle("with-item");
+
+        String path = "test_item.xml";
+        serializer.saveFromPresentation(path, presentation);
+        presentation.clearPresentation();
+        serializer.loadToPresentation(path, presentation);
+
+        Slide loaded = presentation.getSlides().get(0);
+        assertEquals(1, loaded.getSlideItems().size());
+        assertEquals("Hello World", loaded.getSlideItems().get(0).getText());
+        assertEquals(2, loaded.getSlideItems().get(0).getLevel());
+        new File(path).delete();
+    }
+
+    @Test
+    void testSaveAndLoad_MultipleSlides_ShouldRestoreAll() throws Exception
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Slide slide = new Slide();
+            slide.setTitle("Slide " + i);
+            presentation.getSlides().add(slide);
+        }
+        presentation.setTitle("multi");
+
+        String path = "test_multi.xml";
+        serializer.saveFromPresentation(path, presentation);
+        presentation.clearPresentation();
+        serializer.loadToPresentation(path, presentation);
+
+        assertEquals(3, presentation.getSlides().size());
+        new File(path).delete();
+    }
+
+    @Test
     void testLoadToPresentation_NonExistentFile_ShouldThrow()
     {
         assertThrows(Exception.class, () ->
